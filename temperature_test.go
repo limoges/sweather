762eaf0e3562ccd3d7275591b0c019a1509717cf
Julien Limoges <julienlimoges@gmail.com>
2015-06-24 09:38:32 -0400

    Fixed tests

--- temperature_test.go
+++ temperature_test.go
@@ -1,13 +1,42 @@
 package weather
 
 import (
-	"reflect"
+	"math"
 	"testing"
 )
 
-func assert(t *testing.T, got, expected interface{}) {
-	if reflect.DeepEqual(got, expected) {
-		t.Errorf("got %v, expected %v")
+type valuable interface {
+	Value() float64
+}
+
+func assert(t *testing.T, i int, got, expected valuable) {
+	if !nearlyEqual(got.Value(), expected.Value(), 1e-4) {
+		t.Errorf("[%d] got %v, expected %v", i, got.Value(), expected.Value())
+	}
+}
+
+func nearlyEqual(a, b, epsilon float64) bool {
+	// Copied and translated straight from
+	// http://floating-point-gui.de/errors/comparison/
+	diff := math.Abs(a - b)
+	const (
+		MinNormal float64 = 2.2250738585072014E-308 // IEEE-754 Double Precision
+		MaxValue  float64 = math.MaxFloat64
+	)
+	if a == b {
+		// shortcut, handles infinities
+		return true
+
+	} else if a == 0 || b == 0 || diff < MinNormal {
+		// a or b is zero or both are extremely close to it
+		// relative error is less meaningful here
+		return diff < (epsilon * MinNormal)
+
+	} else {
+		// Relative error
+		absA := math.Abs(a)
+		absB := math.Abs(b)
+		return diff/math.Min(absA+absB, MaxValue) < epsilon
 	}
 }
 
@@ -18,20 +47,20 @@ func TestTemperature(t *testing.T) {
 		c Celsius
 		f Fahrenheit
 	}{
-		{k: Kelvin(245.123), c: Celsius(0.2), f: Fahrenheit(0)},
+		{k: Kelvin(245.123), c: Celsius(-28.027), f: Fahrenheit(-18.4486)},
 	}
 
-	for _, test := range tests {
-		assert(t, test.k.Kelvin(), test.k)
-		assert(t, test.k.Celsius(), test.c)
-		assert(t, test.k.Fahrenheit(), test.f)
+	for i, test := range tests {
+		assert(t, i, test.k.Kelvin(), test.k)
+		assert(t, i, test.k.Celsius(), test.c)
+		assert(t, i, test.k.Fahrenheit(), test.f)
 
-		assert(t, test.c.Kelvin(), test.k)
-		assert(t, test.c.Celsius(), test.c)
-		assert(t, test.c.Fahrenheit(), test.f)
+		assert(t, i, test.c.Kelvin(), test.k)
+		assert(t, i, test.c.Celsius(), test.c)
+		assert(t, i, test.c.Fahrenheit(), test.f)
 
-		assert(t, test.f.Kelvin(), test.k)
-		assert(t, test.f.Celsius(), test.c)
-		assert(t, test.f.Fahrenheit(), test.f)
+		assert(t, i, test.f.Kelvin(), test.k)
+		assert(t, i, test.f.Celsius(), test.c)
+		assert(t, i, test.f.Fahrenheit(), test.f)
 	}
 }
